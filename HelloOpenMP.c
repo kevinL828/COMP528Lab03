7213464f77c58359d671954793c42c1fa1460781
kevinL828 <146279052+kevinL828@users.noreply.github.com>
2023-10-17 11:33:47 +0100

    Update HelloOpenMP.c

--- HelloOpenMP.c
+++ HelloOpenMP.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <omp.h>
 
+
 int main(int argc, char const* argv[]){
     int nthreads, thread_id;
+    int i;
+    int arr[5] = {1,3,5,7,9};
     nthreads = omp_get_num_threads();
     thread_id = omp_get_thread_num();
     printf("Goodbye slow serial world and Hello OpenMP!\n");
     printf("I have %d thread(s) and my thread id is %d\n", nthreads, thread_id);
+    #pragma omp parallel for shared(arr) private(i) {
+        for (i = 0; i< 5; i++){
+            printf("%d ", i+2);
+        }
+    }
     return 0;
 }
 
